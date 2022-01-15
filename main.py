f569e360ca15f9f8b1a9eebf04962509210eb027
vladutsenko <87583613+vladutsenko@users.noreply.github.com>
2022-01-15 20:38:39 +0300

    Герой умеет перемещаться по полю

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import pygame
+import os
+
+
+def background():  # отрисовывание поля
+    screen.fill((255, 204, 0), pygame.Rect(10, 20, 700, 560))
+    # вертикальные линии
+    for i in range(0, 11):
+        pygame.draw.line(screen, (255, 0, 0), (70 * i + 10, 20),
+                         (70 * i + 10, 580), width=5)
+    # горизонтальные линии
+    for i in range(0, 9):
+        pygame.draw.line(screen, (255, 0, 0), (10, 70 * i + 20),
+                         (710, 70 * i + 20), width=5)
+
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Pokemon Yellow')
     size = width, height = 1000, 750
     screen = pygame.display.set_mode(size)
-
     screen.fill((204, 204, 204))
-    screen.fill((255, 204, 0), pygame.Rect(10, 20, 700, 560))
+    background()
 
-    # вертикальные линии
-    for i in range(0, 11):
-        pygame.draw.line(screen, (255, 0, 0), (70 * i + 10, 20),
-                         (70 * i + 10, 580), width=5)
-
-    # горизонтальные линии
-    for i in range(0, 9):
-        pygame.draw.line(screen, (255, 0, 0), (10, 70 * i + 20),
-                         (710, 70 * i + 20), width=5)
-
-    pygame.display.flip()
+    # перемещение героя
+    fullname = os.path.join('data', "trainer.png")
+    image = pygame.image.load(fullname)
+    screen.blit(image, (10, 20))
+    pos_x = 10
+    pos_y = 20
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if pos_x > 10:
+                        pos_x = pos_x - 70
+                        background()
+                        screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_RIGHT:
+                    if pos_x < 640:
+                        pos_x = pos_x + 70
+                        background()
+                        screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_UP:
+                    if pos_y > 20:
+                        pos_y = pos_y - 70
+                        background()
+                        screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_DOWN:
+                    if pos_y < 510:
+                        pos_y = pos_y + 70
+                        background()
+                        screen.blit(image, (pos_x, pos_y))
+        pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
     pygame.quit()
