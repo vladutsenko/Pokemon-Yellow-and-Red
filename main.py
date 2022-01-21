c6c6ec72033661b2d727355288f138726bd24fc7
vladutsenko <87583613+vladutsenko@users.noreply.github.com>
2022-01-21 21:10:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,199 +1,213 @@
-import sqlite3
 import pygame
-from random import randrange, choice
+import os
+import sqlite3
+from random import randrange
 
-screen1 = pygame.display.set_mode((680, 400))
+from capture import catch
+from backpack import display
+from help import info
+from shop import buy
 
 
-def catch(region, pokeball):
-    global screen1
-    pygame.display.set_caption('Catch the Pokemon!')
-    screen1 = pygame.display.set_mode((680, 400))
+class Field:
+    def __init__(self, n, m):
+        self.rows = n
+        self.cols = m
+        self.grid = [[0] * m for _ in range(n)]
+        self.place()
+
+    def place(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                r = randrange(100)
+                if r < 20:
+                    self.grid[i][j] = 1
+                elif 20 <= r < 30:
+                    self.grid[i][j] = 2  # ловушка
+                elif 30 <= r < 40:
+                    self.grid[i][j] = 3  # ладан
+
+    def render(self, screen):
+        screen.fill((255, 204, 0), pygame.Rect(10, 20, 700, 560))
+        # вертикальные линии
+        for i in range(0, self.cols):
+            pygame.draw.line(screen, (255, 0, 0), (70 * i + 10, 20),
+                             (70 * i + 10, 580), width=5)
+        # горизонтальные линии
+        for i in range(0, self.rows):
+            pygame.draw.line(screen, (255, 0, 0), (10, 70 * i + 20),
+                             (710, 70 * i + 20), width=5)
+
+
+field = Field(10, 11)
+
+
+def background(region):
+    size = 1000, 600
     pygame.init()
+    screen = pygame.display.set_mode(size)
+    bg = pygame.image.load("data/Pallet-Town.png")
+    screen.blit(bg, (0, 0))
 
-    elements = []
+    field.render(screen)
+    shop = pygame.image.load("data/pokecenter.png")
+    if region == "Kanto":
+        font = pygame.font.Font("data/corbell.ttf", 35)
+        font.bold = True
+        text = font.render("Паллет-таун", True, (0, 0, 0))
+        screen.blit(text, (750, 40))
+        screen.blit(shop, (500, 230))
+    elif region == "Johto":
+        font = pygame.font.Font("data/corbell.ttf", 25)
+        font.bold = True
+        text = font.render("Петалбург-Сити", True, (0, 0, 0))
+        screen.blit(text, (750, 40))
+        screen.blit(shop, (640, 510))
+    elif region == "Hoenn":
+        font = pygame.font.Font("data/corbell.ttf", 35)
+        font.bold = True
+        text = font.render("Литлрут-Таун", True, (0, 0, 0))
+        screen.blit(text, (750, 40))
+        screen.blit(shop, (150, 440))
+
+    image = pygame.image.load("data/backpack.png")
+    screen.blit(image, (870, 100))
+    image = pygame.image.load("data/question-mark.png")
+    screen.blit(image, (870, 480))
+    font = pygame.font.Font("data/corbell.ttf", 50)
+    font.bold = True
     con = sqlite3.connect("Pokemon.db")
     cur = con.cursor()
-    if region == "Kanto":
-        elements = list(cur.execute(
-            f"SELECT name, element, rarity FROM Kanto WHERE name NOT IN (SELECT name FROM Collection)").fetchall())
-    elif region == "Johto":
-        elements = list(cur.execute(
-            f"SELECT name, element, rarity FROM Johto WHERE name NOT IN (SELECT name FROM Collection)").fetchall())
-    elif region == "Hoenn":
-        elements = list(cur.execute(
-            f"SELECT name, element, rarity FROM Hoenn WHERE name NOT IN (SELECT name FROM Collection)").fetchall())
-    pokemon = list(choice(elements))
-    bg = pygame.image.load("data/battle-background.png")
-    screen1.blit(bg, (0, 0))
-    font = pygame.font.Font("data/corbell.ttf", 20)
+    caught = len(list(cur.execute("SELECT * FROM Collection").fetchall()))
+    quantity = len(list(cur.execute("SELECT * FROM Kanto").fetchall())) + len(list(cur.execute(
+        "SELECT * FROM Johto").fetchall())) + len(list(cur.execute("SELECT * FROM Hoenn").fetchall()))
+    font = pygame.font.Font("data/corbell.ttf", 35)
     font.bold = True
-    text = font.render(
-        f"Вы встретили {pokemon[2][:-2]}ого покемона: ", True, (227, 8, 0))
-    screen1.blit(text, (40, 20))
-    text = font.render(f"{pokemon[0]} ({pokemon[1]})", True, (227, 8, 0))
-    screen1.blit(text, (40, 40))
-    text = font.render("Выберите своего покемона:", True, (227, 8, 0))
-    screen1.blit(text, (40, 70))
-    pokemons = cur.execute("SELECT name, element FROM Collection").fetchall()
+    text = font.render("Поймано", True, (0, 0, 0))
+    screen.blit(text, (730, 100))
+    text = font.render(f"       {caught}", True, (0, 0, 0))
+    screen.blit(text, (730, 135))
+    text = font.render("      из", True, (0, 0, 0))
+    screen.blit(text, (730, 170))
+    text = font.render(f"      {quantity + 1}", True, (0, 0, 0))
+    screen.blit(text, (730, 205))
+    text = font.render("покемонов", True, (0, 0, 0))
+    screen.blit(text, (730, 240))
+
+
+def hello():
+    pygame.init()
+    pygame.display.set_caption('Pokemon Yellow and Red')
+    size = 900, 600
+    screen = pygame.display.set_mode(size)
+    bg = pygame.image.load("data/Pallet-Town.png")
+    screen.blit(bg, (0, 0))
+    intro_text = ["      ВЫБЕРИТЕ РЕГИОН", "",
+                  "      Канто", "      Джото", "      Хоенн"]
     image = pygame.image.load("data/pokeball.png")
-    font = pygame.font.Font("data/corbell.ttf", 20)
+    screen.blit(image, (10, 220))
+    screen.blit(image, (10, 295))
+    screen.blit(image, (10, 370))
+    font = pygame.font.Font("data/corbell.ttf", 60)
+
     font.bold = True
-    for i, line in enumerate(pokemons):
-        text = font.render(line[0] + "," + line[1], True, (227, 8, 0))
-        screen1.blit(text, (100, 110 + 50 * i))
-        screen1.blit(image, (40, 100 + 50 * i))
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, True, (0, 0, 255))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height + 5
+        screen.blit(string_rendered, intro_rect)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                for i, p in enumerate(pokemons):
-                    if 110 + 50 * i <= y < 160 + 50 * i and 40 < x < 140:
-                        battle(pokemon, p, region, pokeball)
-                        running = False
-                        break
-        if not running:
-            break
+                exit(0)
+            if event.type == pygame.MOUSEBUTTONDOWN and 10 <= list(event.pos)[0] <= 90:
+                if 200 <= list(event.pos)[1] <= 280:
+                    basic("Kanto")
+                elif 290 <= list(event.pos)[1] <= 370:
+                    basic("Johto")
+                elif 380 <= list(event.pos)[1] <= 460:
+                    basic("Hoenn")
+            pygame.display.flip()
+
+
+def basic(region, x=10, y=20):
+    size = 900, 600
+    screen = pygame.display.set_mode(size)
+    background(region)
+    pygame.init()
+    # перемещение героя
+    image = pygame.image.load("data/trainer.png")
+    screen.blit(image, (x, y))
+    pos_x = x
+    pos_y = y
+    i, j = 0, 0
+    pokeball = 10
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                exit(0)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and pos_x > 10:
+                    pos_x -= 70
+                    j -= 1
+                    background(region)
+                    screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_RIGHT and pos_x < 640:
+                    pos_x += 70
+                    j += 1
+                    background(region)
+                    screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_UP and pos_y > 20:
+                    pos_y -= 70
+                    i -= 1
+                    background(region)
+                    screen.blit(image, (pos_x, pos_y))
+                if event.key == pygame.K_DOWN and pos_y < 510:
+                    pos_y += 70
+                    i += 1
+                    background(region)
+                    screen.blit(image, (pos_x, pos_y))
+                if (region == "Kanto" and pos_x == 500 and pos_y == 230) or \
+                        (region == "Johto" and pos_x == 640 and pos_y == 510) or \
+                        (region == "Hoenn" and pos_x == 150 and pos_y == 440):
+                    buy(pokeball)
+                    basic(region, pos_x, pos_y)
+                if field.grid[i][j] == 1:  # есть покемон или нет
+                    pokeball = catch(region, pokeball)
+                    basic(region, pos_x, pos_y)
+                elif field.grid[i][j] == 2:  # ловушка
+                    pass
+                elif field.grid[i][j] == 3:  # ладан
+                    pass
+            if event.type == pygame.MOUSEBUTTONDOWN and 750 <= list(event.pos)[0] <= 1000 and \
+                    40 <= list(event.pos)[1] <= 100:
+                hello()
+            elif event.type == pygame.MOUSEBUTTONDOWN and 730 <= list(event.pos)[0] <= 1000 and \
+                    100 <= list(event.pos)[1] <= 260:
+                display()
+                basic(region, pos_x, pos_y)
+            elif event.type == pygame.MOUSEBUTTONDOWN and 730 <= list(event.pos)[0] <= 1000 and \
+                    480 <= list(event.pos)[1] <= 580:
+                info()
+                basic(region, pos_x, pos_y)
         pygame.display.flip()
+
+
+if __name__ == '__main__':
+    c = sqlite3.connect("Pokemon.db")
+    cur = c.cursor()
+    cur.execute("DELETE FROM Collection WHERE name != 'Бульбазавр' ")
+    c.commit()
+    c.close()
+    hello()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
     pygame.quit()
-
-
-def battle(pokemon1, pokemon2, region, pokeball):
-    global screen1
-    atk = 1000
-    crit_rate = 15
-    crit_dmg = 50
-    bonus_dmg = 0
-    if pokemon2[0] != "Бульбазавр":
-        c = sqlite3.connect("Pokemon.db")
-        cur = c.cursor()
-        stats = cur.execute(f"SELECT atk, crit_rate, crit_dmg FROM {region} WHERE name = '{pokemon2[0]}'").fetchall()[0]
-        atk, crit_rate, crit_dmg = stats
-    if pokemon1[1] == "травяной" and pokemon2[1] == "огненный":
-        atk *= 1.25
-    elif pokemon1[1] == "огненный" and pokemon2[1] == "водный":
-        bonus_dmg = 100
-    elif pokemon1[1] == "водный" and pokemon2[1] == "огненный":
-        bonus_dmg = 50
-    elif pokemon1[1] == "насекомое" and pokemon2[1] == "нормальный":
-        crit_dmg += 20
-    elif pokemon1[1] == "насекомое" and pokemon2[1] == "водный":
-        crit_rate += 15
-    elif pokemon1[1] == "травяной" and pokemon2[1] == "насекомое":
-        crit_rate -= 5
-        crit_dmg += 50
-    image1 = pygame.image.load(f"data/{pokemon1[0]}.png")
-    image2 = pygame.image.load(f"data/{pokemon2[0]}.png")
-    screen1.blit(image1, (400, 100))
-    screen1.blit(image2, (550, 100))
-    pygame.draw.rect(screen1, (255, 0, 0), (400, 90, 90, 5))
-    pygame.draw.rect(screen1, (255, 0, 0), (550, 90, 90, 5))
-    hp1 = 10000
-    hp2 = 10000
-    pygame.draw.rect(screen1, (255, 0, 0), (450, 220, 80, 40))
-    font = pygame.font.Font("data/corbell.ttf", 20)
-    font.bold = True
-    text = font.render("Атака", True, (0, 0, 0))
-    screen1.blit(text, (465, 230))
-    running = True
-    win = False
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # print("hit")
-                x, y = event.pos
-                if 450 < x < 530 and 220 < y < 260:
-                    dmg1 = (atk if randrange(100) > crit_rate else atk * (1 + crit_dmg / 100)) \
-                           * (1 + bonus_dmg / 100)
-                    dmg2 = 1100
-                    pygame.draw.rect(screen1, (92, 215, 90),
-                                     (490 - min(90.0, dmg1 / hp1 * 90), 90, min(90.0, dmg1 / hp1 * 90), 5))
-                    hp1 -= dmg1
-                    if hp1 <= 0 and hp2:
-                        win = True
-                        running = False
-                    pygame.draw.rect(screen1, (92, 215, 90),
-                                     (640 - min(90.0, dmg2 / hp2 * 90), 90, min(90.0, dmg2 / hp2 * 90), 5))
-                    hp2 -= dmg2
-                    if hp1 and hp2 <= 0:
-                        running = False
-            pygame.display.flip()
-    if win:
-        text = font.render("Победа", True, (0, 0, 0))
-        screen1.blit(text, (460, 325))
-        pygame.display.flip()
-        pygame.time.delay(2000)
-        bg = pygame.image.load("data/battle-background.png")
-        screen1.blit(bg, (0, 0))
-        font = pygame.font.Font("data/corbell.ttf", 20)
-        font.bold = True
-        text = font.render("Выбрите свой покеболл:", True, (0, 0, 0))
-        screen1.blit(text, (20, 20))
-        f = pygame.image.load("data/red.png")
-        screen1.blit(f, (20, 50))
-        f = pygame.image.load("data/blue.png")
-        screen1.blit(f, (220, 50))
-        f = pygame.image.load("data/yellow.png")
-        screen1.blit(f, (420, 50))
-        pygame.display.flip()
-        font = pygame.font.Font("data/corbell.ttf", 20)
-        font.bold = True
-        text2 = font.render("Покемон пойман", True, (0, 0, 0))
-        text3 = font.render("Покемон сбежал", True, (0, 0, 0))
-        text4 = font.render("У вас нет этого покеболла", True, (0, 0, 0))
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    x, y = event.pos
-                    if 5 < y < 150:
-                        bg = pygame.image.load("data/battle-background.png")
-                        screen1.blit(bg, (0, 0))
-                        font = pygame.font.Font("data/corbell.ttf", 20)
-                        font.bold = True
-                        text = font.render(
-                            "Выбрите свой покеболл:", True, (0, 0, 0))
-                        screen1.blit(text, (20, 20))
-                        f = pygame.image.load("data/red.png")
-                        screen1.blit(f, (20, 50))
-                        f = pygame.image.load("data/blue.png")
-                        screen1.blit(f, (220, 50))
-                        f = pygame.image.load("data/yellow.png")
-                        screen1.blit(f, (420, 50))
-                        pygame.display.flip()
-                        if 20 < x < 120 and pokeball > 0:
-                            f = randrange(100)
-                            if f <= 60:
-                                screen1.blit(text2, (20, 300))
-                                con = sqlite3.connect("Pokemon.db")
-                                cur = con.cursor()
-                                cur.execute(
-                                    f"INSERT INTO Collection (name, element) VALUES ('{pokemon1[0]}', '{pokemon1[1]}')")
-                                con.commit()
-                                con.close()
-                                running = False
-                                pokeball = pokeball - 1
-                                break
-                            else:
-                                screen1.blit(text3, (20, 300))
-                                running = False
-                                break
-                        elif 220 < x < 320 or 420 < x < 520:
-                            screen1.blit(text4, (20, 300))
-            pygame.display.flip()
-        pygame.time.delay(2000)
-        pygame.quit()
-
-    else:
-        text = font.render("Поражение", True, (0, 255, 0))
-        screen1.blit(text, (460, 325))
-        pygame.display.flip()
-        pygame.quit()
-    return pokeball
